1610d159326e992ee3e7bd0f0d24c3848e104927
Nesewebel <nesewebel@gmail.com>
2020-03-21 02:01:08 +0100

    Update profile page, design navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
       <router-link to="/profile">Profile</router-link>
     </div>
     <router-view/>
@@ -10,24 +10,45 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #80E6E2;
+    background: #1289A7;
+    
+    height: 100%;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+  }
 
-#nav {
-  padding: 30px;
+  #nav {
+    background: #80E6E2;
+    box-shadow: 0px 0px 4px #2c3e50;
+    margin: 1em;
+    border-radius: .25em;
+    overflow: hidden;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    display: flex;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    a {
+      text-decoration: none;
+      padding: 1em;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #80E6E2;
+        background: #2c3e50;
+      }
     }
   }
-}
+</style>
+
+<style lang="scss">
+  body, html {
+    margin: 0;
+    height: 100%;
+  }
 </style>
