2bbeaab3b79cb4d98556b8760aa457ca686129a7
Robin Lindner <robin@deeprobin.de>
2020-03-21 09:11:51 +0100

    Changed font to 'Raleway'

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,14 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;400&display=swap');
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Raleway', Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #80E6E2;
-    background: #1289A7;
+    //background: #1289A7;
     
     height: 100%;
     overflow: auto;
