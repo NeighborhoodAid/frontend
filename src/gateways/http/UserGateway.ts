1e48b6dff3df7ddb1ff10ea9bb80d8a74b6d6503
delorie <ls@lennart-sommerfeld.de>
2020-03-21 23:10:41 +0100

    add UserGateway

---
+++ src/gateways/http/UserGateway.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export class UserGateway {
+
+    /**
+     * @param email
+     * @param password
+     */
+    login(email: string, password: string): void {
+        // ToDo: not finished implemented
+        axios.post('http://.../api/v1//login', {data: {email: email, password: password}});
+    }
+
+    /**
+     * @param email
+     * @param password
+     * @param mailAddress
+     * @param phoneNumber
+     */
+    signup(email: string, password: string, mailAddress: string, phoneNumber: string): void {
+        // ToDo: not finished implemented
+        axios.post('http://.../api/v1//signup', {data: {email: email, password: password, mailAddress: mailAddress, phoneNumber: phoneNumber}});
+    }
+}
