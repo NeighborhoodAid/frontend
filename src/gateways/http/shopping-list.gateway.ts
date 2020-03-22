fc244189362b05b39b8d04ff78f9e5f8ddf2b794
delorie <ls@lennart-sommerfeld.de>
2020-03-22 09:27:26 +0100

    first version of ShoppingListGateway.getList

--- src/gateways/http/shopping-list.gateway.ts
+++ src/gateways/http/shopping-list.gateway.ts
@@ -1,31 +1,51 @@
 import {ServerGateway} from "@/gateways/http/server.gateway";
 import {UserModel} from "@/models/user.model";
+import {ShoppingListModel} from "@/models/shopping-list.model";
+import {ArticleModel} from "@/models/article.model";
 
 export class ShoppingListGateway {
 
     constructor(private httpClient: ServerGateway) {}
 
-    getList(identifier: string, user: UserModel): void {
+    getList(identifier: string): void {
         // ToDo: not finished implemented
-        this.httpClient.getHttpInstance().get('http://.../api/v1/list/'+identifier);
+        this.httpClient.getHttpInstance().get('http://.../api/v1/list/'+identifier).then((response) => {
+
+            let articleList = [];
+            for (let articleItem of response.data.articles) {
+                articleList.push(new ArticleModel(articleItem.amount, articleItem.title, articleItem.description))
+            }
+
+            const shoppingList = new ShoppingListModel(
+                response.data.id,
+                response.data.creator,
+                response.data.claimer,
+                response.data.creationDateTime,
+                response.data.dueDateTime,
+                articleList
+            );
+
+            // add to store
+            // commit("setShoppingLists", shoppingList)
+        });
     }
 
-    create(user: UserModel): void {
+    create(): void {
         // ToDo: not finished implemented
         this.httpClient.getHttpInstance().post('http://.../api/v1/list', {data: {}});
     }
 
-    update(identifier: string, user: UserModel) : void {
+    update(identifier: string) : void {
         // ToDo: not finished implemented
         this.httpClient.getHttpInstance().put('http://.../api/v1/list/'+identifier, {data: {}});
     }
 
-    claim(identifier: string, user: UserModel): void  {
+    claim(identifier: string): void  {
         // ToDo: not finished implemented
         this.httpClient.getHttpInstance().post('http://.../api/v1/list/'+identifier+'/claim', {data: {}});
     }
 
-    delete(identifier: string, user: UserModel): void {
+    delete(identifier: string): void {
         // ToDo: not finished implemented
         this.httpClient.getHttpInstance().delete('http://.../api/v1/list/'+identifier);
     }
