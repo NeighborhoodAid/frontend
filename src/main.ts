26f24aa7753b0aa446ea56e62c6b4762c7b4e342
Nathan <nathan.nr@outlook.com>
2020-03-22 19:07:44 +0100

    Add icons to navbar

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+import {library} from "@fortawesome/fontawesome-svg-core"
+import * as fa from "@fortawesome/free-solid-svg-icons"
+
+library.add(fa.faCartPlus)
+library.add(fa.faCheck)
+library.add(fa.faInfo)
+library.add(fa.faPlay)
+library.add(fa.faUser)
+library.add(fa.faUserEdit)
+library.add(fa.faUserPlus)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
