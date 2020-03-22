2fa2fdc28c1ed8ce31f9e63a565a4cfdf0a9a90e
Nathan <nathan.nr@outlook.com>
2020-03-22 20:39:46 +0100

    Add text for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>NeighborhoodAid</h1>
+    <p>NeighborhoodAid ist eine Anwendung, die es Menschen, welche nicht selbst einkaufen können, weil Sie zum
+        Beispiel zur Risikogruppe gehören, ermöglicht, ihren Einkaufszettel online zu erstellen.</p>
+    <p>Über unsere Webseite haben andere Personen, welche helfen wollen, dann die Möglichkeit Einkaufslisten von
+        betroffenen Personen in der Nähe zu sehen und den Einkauf für diese zu erledigen.</p>
+    <p>Dieser Prototyp wurde im Rahmen des Hackatons der Bundesregierung gegen den Coronavirus <a
+            href="https://wirvsvirushackathon.org/">#WirVsVirus</a> vom 20. bis zum 22. März 2020 erstellt.</p>
+    <p>Die Anwendung ist vollständig Open-Source, der Quellcode ist auf <a
+            href="https://github.com/NeighborhoodAid">GitHub</a> verfügbar.</p>
   </div>
 </template>
+
+<style scoped lang="scss">
+    a {
+        color: #42b983;
+    }
+</style>
